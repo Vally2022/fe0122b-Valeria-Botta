f08e82681a6fd97b2ff9cd7b120b337918471b2e
Vally2022 <valeriabotta@ymail.com>
2022-02-18 14:03:16 +0100

    progetto calcolatrice

--- s4/progetto-g5/myscripts/script.js
+++ s4/progetto-g5/myscripts/script.js
@@ -1,15 +1,34 @@
+var operazione = false;
+
+
 function tasto(valore) {
-var display = document.getElementById('risultato').value;
-document.getElementById('risultato').value += valore;
+    
+    if (valore == "+" || valore == "*" || valore == "-" || valore == "/") {
+        if (operazione == false) {
+            document.getElementById("risultato").value += valore;
+            operazione = true;
+        }
+    }
+
+    if (Number.isInteger(valore) || valore == '.') {
+        document.getElementById("risultato").value += valore;
+    }
 }
+
 
 function uguale() {
     document.getElementById("risultato").value = eval(document.getElementById("risultato").value);
+    operazione = false;
 
 }
+//funge da reset
+function reset() {
+    document.getElementById("risultato").value = "";
+    operazione = false;
+}
 
+//funge da cancella per singolo numero
 function cancella() {
-    document.getElementById('risultato').value = "";
+    document.getElementById("risultato").value = document.getElementById("risultato").value.substr(0, document.getElementById("risultato").value.length - 1);
+    operazione = false;
 }
-
-
